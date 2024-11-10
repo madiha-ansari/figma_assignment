6690e60eda17c63909b4b7ac014d9ad8031802d9
madiha-ansari <157696198+madiha-ansari@users.noreply.github.com>
2024-11-10 18:59:42 +0500

    img

---
+++ app/components/header/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Page = () => {
+    return (
+        <div className='w-[135px] h-[72px] bg-headerBackground'>
+
+            <div className='mx-8 w-[439px] h-[94px] top-20px left-36 font-algerian font-normal text-[49px] line-height:93.75px text-white'>
+                MANZZARI
+            </div>
+
+        </div>
+    )
+}
+
+export default Page
